d3663a221384712933c2286f61f2f731b95c1ebf
brendonstahl97 <brendonstahl97@gmail.com>
2020-11-24 15:04:35 -0500

    added functions to view roles and departments

--- Assets/js/Options.js
+++ Assets/js/Options.js
@@ -14,38 +14,92 @@ class Options {
             {
                 type: 'list',
                 message: 'What would you like to do?',
-                choices: ['View all Employees'],
+                choices: ['View all Employees', 'View Departments', 'View Roles', 'Add Department', 'Add Employee', 'Add Role', 'Exit'],
                 name: 'option'
             }
         ])
 
         this.option = option;
+        console.log(this.option);
         this.enactOption();
     }
 
     // selects appropriate method based on user selection
     enactOption() {
 
-        switch(this.option) {
+        switch (this.option) {
             case 'View all Employees':
-                console.log("viewing all employees")
                 this.viewAllEmployees();
+                break;
+            case 'View Departments':
+                this.viewDepartments();
+                break;
+            case 'View Roles':
+                this.viewRoles();
+                break;
+            case 'Add Department':
+                this.addDepartment();
+                break;
+            case 'Add Employee':
+                this.addEmployee();
+                break;
+            case 'Add Role':
+                this.addRole();
+                break;
+            case 'Exit':
+                this.connection.end();
                 break;
         }
     }
 
     viewAllEmployees() {
-        console.log("all Employees");
         this.connection.query(`SELECT employee.first_name AS 'First Name', employee.last_name AS 'Last Name', role.title AS 'Role', role.salary AS 'Salary', department.name AS 'Department', CONCAT(managers.first_name, ' ', managers.last_name) AS 'Manager'
         FROM employee 
         INNER JOIN role ON employee.role_id = role.id 
         INNER JOIN department ON role.department_id = department.id 
         LEFT JOIN employee AS managers On employee.manager_id = managers.id;`, (err, res) => {
             if (err) throw err;
+            console.log('\n');
             console.table(res);
+            console.log('\n');
+            this.getOption();
+        })
+    }
+
+    viewDepartments() {
+        console.log('mega Dept call');
+        this.connection.query(`SELECT name AS 'Department' FROM department;`, (err, res) => {
+            if (err) throw err;
+            console.log('\n');
+            console.table(res);
+            console.log('\n');
+            this.getOption();
         })
 
-        this.getOption();
+
+    }
+
+    viewRoles() {
+        this.connection.query(`SELECT role.title AS 'Role', role.salary AS 'Salary', department.name AS 'Department' FROM role INNER JOIN department ON role.department_id = department.id;`, (err, res) => {
+            if (err) throw err;
+            console.log('\n');
+            console.table(res);
+            console.log('\n');
+            this.getOption();
+        })
+
+    }
+
+    addDepartment() {
+        this.connection.query();
+    }
+
+    addEmployee() {
+        this.connection.query();
+    }
+
+    addRole() {
+        this.connection.query();
     }
 }
 
